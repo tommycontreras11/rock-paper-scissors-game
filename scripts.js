c40caa19066062a3034b5d4ba500f4638f4e39ba
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-01-02 16:01:15 -0400

    feat: :sparkles: add function to get the result of the round

---
+++ scripts.js
@@ -0,0 +1,29 @@
+function getRandomComputerResult() {
+    const options = ["Rock", "Paper", "Scissors"];
+    return options[Math.floor(Math.random() * options.length)];
+}
+
+function hasPlayerWonTheRound(player, computer) {
+    return (
+      (player === "Rock" && computer === "Scissors") ||
+      (player === "Scissors" && computer === "Paper") ||
+      (player === "Paper" && computer === "Rock")
+    );
+}
+
+let playerScore = 0;
+let computerScore = 0;
+
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+
+  if (hasPlayerWonTheRound(userOption, computerResult)) {
+    playerScore++;
+    return `Player wins! ${userOption} beats ${computerResult}`;
+  } else if (computerResult === userOption) {
+    return `It's a tie! Both chose ${userOption}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
+  }
+}
