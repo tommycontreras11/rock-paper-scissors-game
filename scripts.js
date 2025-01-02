860394bad8fd92c8c897679ad4bd51a5ed8918c2
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-01-02 16:01:47 -0400

    feat: :sparkles: add function to show the results

--- scripts.js
+++ scripts.js
@@ -27,3 +27,25 @@ function getRoundResults(userOption) {
     return `Computer wins! ${computerResult} beats ${userOption}`;
   }
 }
+
+const playerScoreSpanElement = document.getElementById("player-score");
+const computerScoreSpanElement = document.getElementById("computer-score");
+const roundResultsMsg = document.getElementById("results-msg");
+const winnerMsgElement = document.getElementById("winner-msg");
+const optionsContainer = document.querySelector(".options-container");
+const resetGameBtn = document.getElementById("reset-game-btn");
+
+function showResults(userOption) {
+  roundResultsMsg.innerText = getRoundResults(userOption);
+  computerScoreSpanElement.innerText = computerScore;
+  playerScoreSpanElement.innerText = playerScore;
+
+  if (playerScore === 3 || computerScore === 3) {
+    winnerMsgElement.innerText = `${
+      playerScore === 3 ? "Player" : "Computer"
+    } has won the game!`;
+
+    resetGameBtn.style.display = "block";
+    optionsContainer.style.display = "none";
+  }
+};
