b4513efb707387d3f48496182ee97789483486a2
Tommy Grullon Contreras <tommycontreras34@gmail.com>
2025-01-02 16:02:43 -0400

    feat: :sparkles: add event listeners to buttons

--- scripts.js
+++ scripts.js
@@ -62,3 +62,21 @@ function resetGame() {
   resetGameBtn.styles.display = "none";
   optionsContainer.styles.display = "block";
 }
+
+resetGameBtn.addEventListener("click", resetGame);
+
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+
+rockBtn.addEventListener("click", function () {
+  showResults("Rock");
+});
+
+paperBtn.addEventListener("click", function () {
+  showResults("Paper");
+});
+
+scissorsBtn.addEventListener("click", function () {
+  showResults("Scissors");
+});
